ba31c9f72be1a2d669a820198ab50e353c9f5b8f
nspboy <nagarajnagaraja.2003@gmail.com>
2025-11-24 17:05:40 +0530

    new things

--- counter/counter.js
+++ counter/counter.js
@@ -1,11 +1,21 @@
 let c = 0;
+let clickcount=0;
 
 function increase() {
+    clickcount++;
+    if(clickcount%2===0){ 
     c++;
     document.getElementById("count").innerText = c;
+    clickcount-0;
+}
 }
 
 function decrease() {
     c--;
+    document.getElementById("count").innerText = c;
+}
+
+function refresh(){
+    c=0;
     document.getElementById("count").innerText = c;
 }
--- password/password.js
+++ password/password.js
@@ -1,4 +1,10 @@
-function togglePassword() {
-    let p = document.getElementById("pass");
-    p.type = p.type === "password" ? "text" : "password";
-}
+// function togglePassword() {
+//     let p = document.getElementById("pass");
+//     p.type = p.type === "password" ? "text" : "password";
+// }
+let show = false;
+function toggle(){
+     show = !show;
+     document.getElementById("hide").innerText = show ? "Hide" : "Show";
+      }
+
